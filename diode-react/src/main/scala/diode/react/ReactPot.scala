8711e1da9fcb74f5bdaa17384695257a435161c8
Rahel Lüthy <rahel.luethy@gmail.com>
2017-03-10 08:22:28 +0100

    Upgrade to scalajs-react v1.0.0-RC1

--- diode-react/src/main/scala/diode/react/ReactPot.scala
+++ diode-react/src/main/scala/diode/react/ReactPot.scala
@@ -1,8 +1,7 @@
 package diode.react
 
 import diode.data.{PendingBase, Pot}
-import diode.util._
-import japgolly.scalajs.react.ReactNode
+import japgolly.scalajs.react.vdom.VdomNode
 
 object ReactPot {
 
@@ -13,38 +12,38 @@ object ReactPot {
     /**
       * Render non-empty (ready or stale) content
       *
-      * @param f Transforms Pot value into a ReactNode
+      * @param f Transforms Pot value into a VdomNode
       * @return
       */
-    def render(f: A => ReactNode): ReactNode =
+    def render(f: A => VdomNode): VdomNode =
       if (pot.nonEmpty) f(pot.get) else null
 
     /**
       * Render content in Ready state, not including stale states
       *
-      * @param f Transforms Pot value into a ReactNode
+      * @param f Transforms Pot value into a VdomNode
       * @return
       */
-    def renderReady(f: A => ReactNode): ReactNode =
+    def renderReady(f: A => VdomNode): VdomNode =
       if (pot.isReady) f(pot.get) else null
 
     /**
       * Render when Pot is pending
       *
-      * @param f Transforms duration time into a ReactNode
+      * @param f Transforms duration time into a VdomNode
       * @return
       */
-    def renderPending(f: Int => ReactNode): ReactNode =
+    def renderPending(f: Int => VdomNode): VdomNode =
       if (pot.isPending) f(pot.asInstanceOf[PendingBase].duration()) else null
 
     /**
       * Render when Pot is pending with a filter on duration
       *
       * @param b Filter based on duration value
-      * @param f Transforms duration time into a ReactNode
+      * @param f Transforms duration time into a VdomNode
       * @return
       */
-    def renderPending(b: Int => Boolean, f: Int => ReactNode): ReactNode = {
+    def renderPending(b: Int => Boolean, f: Int => VdomNode): VdomNode = {
       if (pot.isPending) {
         val duration = pot.asInstanceOf[PendingBase].duration()
         if (b(duration)) f(duration) else null
@@ -54,28 +53,28 @@ object ReactPot {
     /**
       * Render when Pot has failed
       *
-      * @param f Transforms an exception into a ReactNode
+      * @param f Transforms an exception into a VdomNode
       * @return
       */
-    def renderFailed(f: Throwable => ReactNode): ReactNode =
+    def renderFailed(f: Throwable => VdomNode): VdomNode =
       pot.exceptionOption.map(f).orNull
 
     /**
       * Render stale content (`PendingStale` or `FailedStale`)
       *
-      * @param f Transforms Pot value into a ReactNode
+      * @param f Transforms Pot value into a VdomNode
       * @return
       */
-    def renderStale(f: A => ReactNode): ReactNode =
+    def renderStale(f: A => VdomNode): VdomNode =
       if (pot.isStale) f(pot.get) else null
 
     /**
       * Render when Pot is empty
       *
-      * @param f Returns a ReactNode
+      * @param f Returns a VdomNode
       * @return
       */
-    def renderEmpty(f: => ReactNode): ReactNode =
+    def renderEmpty(f: => VdomNode): VdomNode =
       if (pot.isEmpty) f else null
   }
 
