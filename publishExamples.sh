41b5240b627be44ef478cbc5c3cbe4094256c879
Otto Chrons <otto@chrons.me>
2019-05-11 14:00:14 +0300

    Fix todomvc example publishing

--- publishExamples.sh
+++ publishExamples.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 function copyExample {
-  mkdir -p examples/$1 examples/$1/classes
+  mkdir -p examples/$1 examples/$1/classes examples/$1/scalajs-bundler/main
   cp ../examples/$1/target/scala-2.12/*.js examples/$1
   cp ../examples/$1/target/scala-2.12/classes/*.{js,css,png,jpg} examples/$1/classes
+  cp ../examples/$1/target/scala-2.12/scalajs-bundler/main/*opt-bundle.js examples/$1/scalajs-bundler/main
   # Modify index.html to work online
   cat ../examples/$1/target/scala-2.12/classes/index.html | sed -re 's/src="\.\.\//src="/' | sed -re 's/-jsdeps./-jsdeps.min./' | sed -re 's/-fastopt/-opt/' | sed -re 's/.*workbench.js.*//' > examples/$1/index.html
 }
 
-examples=(raf treeview simple todomvc)
+examples=(raf treeview simple)
 
 rm -rf _examples
 mkdir _examples
@@ -21,4 +22,10 @@ do
    popd
    copyExample $i
 done
+
+pushd ../examples/todomvc
+sbt clean fullOptJS::webpack
+popd
+copyExample todomvc
+
 exit
